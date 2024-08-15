2b2bb58f38f070a4ca2cea1ec9fe4ec5f7f95a4e
Marcelo Torres <kevin.torres85@outlook.es>
2024-08-14 20:45:52 -0500

    ejercicio grupal

---
+++ Ejercicio 14-04-2024/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14_04_2024
+{
+    public class Persona
+    {
+        public string nombre;
+        public double estatura;
+        public int edad;
+
+        /* metodo para mostrar la info del usuario
+         * metodo para verificar si el usuario es mayor o menor de edad*/
+
+        public void Informacion( string nombre, double estatura, int edad)
+        {
+            this.nombre = nombre;
+            this.estatura = estatura;
+            this.edad = edad;
+        }
+
+        public void mostrarInfo()
+        {
+            Console.WriteLine($"Su nombre es: {nombre}");
+            Console.WriteLine($"Su estatura es: {estatura}");
+            Console.WriteLine($"Su edad es: {edad}");
+        }
+
+        public void mostrarEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine($"\n{nombre} tienes {edad} años y es MAYOR DE EDAD");
+            }
+            else
+            {
+                Console.WriteLine($"\n{nombre} tienes {edad} años y es MENORDE EDAD");
+            }
+        }
+    }
+}
---
+++ Ejercicio 14-04-2024/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14_04_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+
+            Console.WriteLine("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su estatura en metros");
+            double estatura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            persona.Informacion(nombre, estatura, edad);
+
+            persona.mostrarInfo();
+
+            persona.mostrarEdad();
+            
+            Console.ReadKey();
+        }
+    }
+}
